1ef99e493394cb17fb833bd3c3f212e381d28558
JoycexChan <163320736+JoycexChan@users.noreply.github.com>
2024-04-05 14:31:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function toggleStar(element) {
+    console.log('Star clicked!'); // 在控制台中輸出，用於調試
+    element.classList.toggle('empty');
+}
+
+function toggleMenu() {
+    var popup = document.getElementById("popupMenu");
+    popup.classList.toggle('show-menu'); // 切換 .show-menu 類來控制菜單的顯示和隱藏
+}
+
+// 這個函數可能之前缺失了
+function closeMenu() {
+    var popup = document.getElementById("popupMenu");
+    popup.classList.remove('show-menu'); // 移除 .show-menu 類來隱藏菜單
+}
