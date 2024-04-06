311a3042ec1905c3bb2fa700d59acc051372a693
JoycexChan <163320736+JoycexChan@users.noreply.github.com>
2024-04-06 16:15:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,23 +14,3 @@ function closeMenu() {
     var popup = document.getElementById("popupMenu");
     popup.classList.remove('show-menu'); // 移除 .show-menu 類來隱藏菜單
 }
-
-
-  function adjustViewport() {
-    const width = window.innerWidth;
-    let maxScale = 1; // 預設最大縮放比例
-
-    if (width > 1200 && width <= 1920) {
-      maxScale = 3;
-    } else if (width > 600 && width <= 1200) {
-      maxScale = 1.5; // 假設1200~1920之間最大允許放大到2倍
-    } else if (width > 360 && width <= 600) {
-      maxScale = 1;
-    }
-
-    document.querySelector('meta[name="viewport"]').setAttribute('content', `width=device-width, initial-scale=1, maximum-scale=${maxScale}`);
-  }
-
-  window.addEventListener('load', adjustViewport);
-  window.addEventListener('resize', adjustViewport);
-
